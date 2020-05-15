5e8ad49977e3ac83e2d41e25500ae9c49db7c9a6
IssacYoung2013 <issacyoung@msn.cn>
2020-05-15 12:42:32 +0800

    add user regist

--- foodie-dev-pojo/src/main/java/com/issac/pojo/bo/UserBO.java
+++ foodie-dev-pojo/src/main/java/com/issac/pojo/bo/UserBO.java
@@ -1,13 +1,21 @@
 package com.issac.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @author: ywy
  * @date: 2020-05-12
  * @desc:
  */
+@ApiModel(value = "用户对象BO",description = "从客户端，由用户传入的数据封装在此 entity")
 public class UserBO {
+
+    @ApiModelProperty(value = "用户名",name = "username",example = "issac",required = true)
     private String username;
+    @ApiModelProperty(value = "密码",name = "password",example = "123456",required = true)
     private String password;
+    @ApiModelProperty(value = "确认密码",name = "confirmPassword",example = "123456",required = true)
     private String confirmPassword;
 
     public String getUsername() {
